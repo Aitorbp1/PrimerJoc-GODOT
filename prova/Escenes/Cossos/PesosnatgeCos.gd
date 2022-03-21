276801e2518a13318208fe29c18a654da9ff8675
Aitorbp1 <101257184+Aitorbp1@users.noreply.github.com>
2022-03-21 14:25:10 +0100

    Fer que salti be i que caigui si cal

--- prova/Escenes/Cossos/PesosnatgeCos.gd
+++ prova/Escenes/Cossos/PesosnatgeCos.gd
@@ -4,8 +4,17 @@ var velocitat_base = 200 #la velocitat inicial
 var velocitat = Vector2.ZERO
 var direccio = Vector2.DOWN
 var gravetat = Vector2.DOWN * 980
-
+var velocitat_salt = -300
 
 func _physics_process(delta):
-	move_and_slide(velocitat)
+	velocitat.x = 0
 	velocitat += gravetat * delta
+	if Input.is_action_pressed("mou dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("mou esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("mou amunt") and is_on_floor():
+		velocitat.y = velocitat_salt
+	
+	
+	velocitat = move_and_slide(velocitat, Vector2.UP)
