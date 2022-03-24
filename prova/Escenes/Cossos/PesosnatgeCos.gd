d160f2c0a6cc775dca227861cbad321db380c65d
Aitorbp1 <101257184+Aitorbp1@users.noreply.github.com>
2022-03-24 09:02:00 +0100

    Terres i personatge que saltaa

--- prova/Escenes/Cossos/PesosnatgeCos.gd
+++ prova/Escenes/Cossos/PesosnatgeCos.gd
@@ -4,7 +4,7 @@ var velocitat_base = 200 #la velocitat inicial
 var velocitat = Vector2.ZERO
 var direccio = Vector2.DOWN
 var gravetat = Vector2.DOWN * 980
-var velocitat_salt = -300
+var velocitat_salt = -1000
 
 func _physics_process(delta):
 	velocitat.x = 0
@@ -15,6 +15,25 @@ func _physics_process(delta):
 		velocitat += Vector2.LEFT * velocitat_base
 	if Input.is_action_just_pressed("mou amunt") and is_on_floor():
 		velocitat.y = velocitat_salt
+		
 	
 	
 	velocitat = move_and_slide(velocitat, Vector2.UP)
+	animation(velocitat)
+func animation(velocitat):
+	if velocitat.x > 0 and velocitat.y > -2:
+		$Alieen.play("Camina")
+		$Alieen.flip_h = false
+	elif velocitat.x < 0 and velocitat.y > -2:
+		$Alieen.play("Camina")
+		$Alieen.flip_h = true
+		
+	if abs(velocitat.x) < 0.1 and velocitat.y > -1:
+		$Alieen.play("Quiet")
+		
+	if velocitat.y < -2:
+		$Alieen.play("Salta")
+		
+	
+
+ 
